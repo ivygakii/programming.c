64a5bfeffdf7f149b5b132461c4978dc2974dca4
ivygakii <122874337+ivygakii@users.noreply.github.com>
2023-01-21 12:16:47 +0300

    Add files via upload

---
+++ product.c
@@ -0,0 +1,13 @@
+/*Arithmetic operators multiplication user friendly program */
+#include <stdio.h>
+int main ()
+{
+int a, b, product;
+printf("\n Enter value of A:");
+scanf("%d", &a);
+printf("\n Enter value of B:");
+scanf("%d", &b);
+product=a*b;
+printf("\n PRODUCT=%d", product);
+return 0;
+}
