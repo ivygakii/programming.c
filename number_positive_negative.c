cb54bf7ccc73cee8e26aa111f8105cea867c8954
ivygakii <122874337+ivygakii@users.noreply.github.com>
2023-02-07 11:44:52 +0300

    Add files via upload

---
+++ number_positive_negative.c
@@ -0,0 +1,13 @@
+//program to check whether a number is positive or negative
+#include<stdio.h>
+int main()
+{
+int a;
+printf("\nEnter a value: ");
+scanf("%d" ,&a);
+if(a>=0);{
+  printf("%d is positive:" ,a);}
+else{
+printf("%d is negative:" ,a);}
+return 0;
+}
