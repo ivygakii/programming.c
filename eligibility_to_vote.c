6e1d52ef22013ae3e03e30cf578b3943e5e3286f
ivygakii <122874337+ivygakii@users.noreply.github.com>
2023-02-08 12:48:10 +0300

    Add files via upload

---
+++ eligibility_to_vote.c
@@ -0,0 +1,13 @@
+//program to check if aperon is eligible to vote
+#include <stdio.h>
+int main() {
+int age;
+printf("Enter age of the person:");
+scanf("%d" ,&age);
+if(age<=18);{
+printf("you are not eligible to vote:");
+else
+if(age>=18);
+printf("you are eligible to vote:");
+return 0;
+}
